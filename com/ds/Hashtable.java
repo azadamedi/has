256a8f9070c29f99e14779643a605a2ff5af88bf
azadamedi <58348730+azadamedi@users.noreply.github.com>
2020-04-11 02:07:21 +0300

    Update Hashtable.java

--- com/ds/Hashtable.java
+++ com/ds/Hashtable.java
@@ -3,10 +3,10 @@ package com.ds;
 public class Hashtable {
     public static void main(String[]args){
         Hashtablarray<String>hash=new Hashtablarray<String>(10);
-        hash.put(10,"azad");
-        hash.put(11,"king");
-        hash.put(12,"soz");
-        System.out.print(hash.Get(10));
+        hash.put("a","azad");
+        hash.put("b","king");
+        hash.put("c","soz");
+        System.out.print(hash.Get("c"));
 
     }
 }
